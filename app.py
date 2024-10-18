708cebf86c8d164ed8f992ef2ddbbb2dea86859a
Government Name - <baurcreative@gmail.com>
2024-10-18 08:08:43 -0700

    turned print into Test

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 """Torrent: A simple Python program to print "Hello World"."""
 
-print("Hello World")
 
+
+def test_dummy():
+    print("Hello World")
+    pass
+
+test_dummy()
 # End-of-file (EOF)
